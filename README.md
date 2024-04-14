6db1e434d19afc9ebd43ca3b75a0ed5b8afd6568
some45bucks <jhaightsnake@gmail.com>
2024-04-13 18:11:25 -0600

    test code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # legged_DP_learning
+
+
+## Errors and fixes
+[libGL error: MESA-LOADER: failed to open swrast...](https://stackoverflow.com/questions/72110384/libgl-error-mesa-loader-failed-to-open-iris)
+[Jax Cuda not installed](https://github.com/google/jax/issues/18027)
+
---
+++ test.py
@@ -0,0 +1,70 @@
+from datetime import datetime
+import functools
+import jax
+from IPython.display import HTML
+from jax import numpy as jp
+import numpy as np
+from typing import Any, Dict, Sequence, Tuple, Union
+
+from brax import base
+from brax import envs
+from brax import math
+from brax.base import Base, Motion, Transform
+from brax.envs.base import Env, PipelineEnv, State
+from brax.mjx.base import State as MjxState
+from brax.training.agents.ppo import train as ppo
+from brax.training.agents.ppo import networks as ppo_networks
+from brax.io import html, mjcf, model
+
+from etils import epath
+from flax import struct
+from matplotlib import pyplot as plt
+import mediapy as media
+from ml_collections import config_dict
+import mujoco
+from mujoco import mjx
+
+ctx = mujoco.GLContext(100, 100)
+ctx.make_current()
+xml = """
+<mujoco>
+  <worldbody>
+    <light name="top" pos="0 0 1"/>
+    <body name="box_and_sphere" euler="0 0 -30">
+      <joint name="swing" type="hinge" axis="1 -1 0" pos="-.2 -.2 -.2"/>
+      <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+      <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+    </body>
+  </worldbody>
+</mujoco>
+"""
+
+# Make model, data, and renderer
+mj_model = mujoco.MjModel.from_xml_string(xml)
+mj_data = mujoco.MjData(mj_model)
+renderer = mujoco.Renderer(mj_model)
+
+mjx_model = mjx.put_model(mj_model)
+mjx_data = mjx.put_data(mj_model, mj_data)
+
+print(mj_data.qpos, type(mj_data.qpos))
+print(mjx_data.qpos, type(mjx_data.qpos), mjx_data.qpos.devices())
+
+# enable joint visualization option:
+scene_option = mujoco.MjvOption()
+scene_option.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True
+
+duration = 3.8  # (seconds)
+framerate = 60  # (Hz)
+
+frames = []
+mujoco.mj_resetData(mj_model, mj_data)
+while mj_data.time < duration:
+  mujoco.mj_step(mj_model, mj_data)
+  if len(frames) < mj_data.time * framerate:
+    renderer.update_scene(mj_data, scene_option=scene_option)
+    pixels = renderer.render()
+    frames.append(pixels)
+
+# Simulate and display video.
+media.show_video(frames, fps=framerate)
